7f8d881986707047ace1a7ddcbcc914a97c174c3
DaniloProfita <111090713+DaniloProfita@users.noreply.github.com>
2023-08-24 20:32:46 -0300

    Semana 01

---
+++ Cuarto-Semestre/JavaScript/e-commerce2023/client/js/products.js
@@ -0,0 +1,30 @@
+const productos=[
+    {
+        id: 1,
+        productName: "iPhone SE",
+        price: 200,
+        quanty: 1,
+        img:"/client/media/iPhoneSE2022.webp"    
+    },
+    {
+        id: 2,
+        productName: "iPhone 11",
+        price: 300,
+        quanty: 1,
+        img:"/client/media/iPhone11.webp"    
+    },
+    {
+        id: 3,
+        productName: "iPhone 12",
+        price: 400,
+        quanty: 1,
+        img:"/client/media/iPhone12.webp"    
+    },
+    {
+        id: 4,
+        productName: "iPhone 13",
+        price: 500,
+        quanty: 1,
+        img:"/client/media/iPhone13.webp"
+    }  
+]
